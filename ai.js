fb3ca04cbd8bcf36ff9f822ccb03b121ae4177a9
soulwax <konstantin.kling@braincon.de>
2022-07-11 19:43:58 +0000

    fine tuning premises

--- ai.js
+++ ai.js
@@ -4,15 +4,18 @@ const VERBOSE = process.env.VERBOSE
 const { cleanResultText } = require('./helper')
 const got = require('got')
 
-const premiseJPFemale = 'Shimizu is a lively female japanese teenager living in Tokyo who is very knowledgeable about many things, especially song lyrics and literature'
+const botName = 'Shimizu'
+const botAttributes = 'friendly and conservative Japanese woman in her early twenties living and studying in Tokyo'
+const botAdditionalAttributes = 'nerdy and very knowledgeable about songs, literature, science, metaphysics and philosophy.'
+//const premiseJPFemale = 'Shimizu is a lively female japanese teenager living in Tokyo who is very knowledgeable about many things, especially song lyrics and literature'
 // ... add more premises
-const premises = []
+const premises = [botName, botAttributes, botAdditionalAttributes]
 
 
 const getPrompt = async (prompt, maxTokens) => {
   const url = 'https://api.openai.com/v1/engines/davinci/completions'
   const params = {
-    prompt: `${premiseJPFemale}:\n\n${prompt}`,
+    prompt: `${botName} is a ${botAttributes} who is ${botAdditionalAttributes}:\n\n${prompt}`,
     temperature: 0.6,
     max_tokens: maxTokens,
     top_p: 0.3,
