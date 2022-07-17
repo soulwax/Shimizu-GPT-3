4bc6559517460e0e34a3b521da3bd6307ca3c006
soulwax <konstantin.kling@braincon.de>
2022-07-17 06:06:56 +0000

    Capitalize db docs

--- db.js
+++ db.js
@@ -1,4 +1,11 @@
 require('dotenv').config({ path: __dirname + '/.env' })
+/**
+ * Module exports a function that creates a new Conversation object in the database.
+ * 
+ * @param {Object} mongoose 
+ * @param {String} connectionString 
+ * @returns {Object} return the mongoose connection
+ */
 
 module.exports = (mongoose, connectionString) => {
   mongoose.connect(connectionString, { useNewUrlParser: true })
