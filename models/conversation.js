8fad173d0b53d40cc30e0196ce04b18f070ec002
soulwax <konstantin.kling@braincon.de>
2022-07-24 17:36:41 +0000

    Refining the conversation schema

--- models/conversation.js
+++ models/conversation.js
@@ -5,22 +5,53 @@
  */
 module.exports = (mongoose) => {
   const conversationSchema = new mongoose.Schema({
-    type: String,
-    guildID: Number,
+    type: {
+      type: String,
+      enum: ['private', 'channel'],
+      default: 'channel'
+    },
+    guildID: {
+      type: String,
+      required: true
+    },
     conversations: [
       {
-        conversationID: Number,
-        userID: Number,
-        userName: String,
+        conversationID: {
+          type: Number,
+          required: true
+        },
+        userID: {
+          type: String,
+          required: true
+        },
+        userName: {
+          type: String,
+          required: true,
+          default: 'Unknown'
+        },
         messages: [
           {
-            messageID: Number,
-            message: String,
-            timestamp: Date
+            messageID: {
+              type: Number,
+              required: true
+            },
+            message: {
+              type: String,
+              required: true,
+              default: ''
+            },
+            timestamp: {
+              type: Date,
+              required: true,
+              default: Date.now
+            }
           }
         ]
       }
     ]
   })
+
+  //TODO: define helper functions for the conversation model
+
   return mongoose.model('Conversation', conversationSchema)
 }
